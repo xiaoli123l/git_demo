e9f84609cba9924c75e883e4d3575dbd1cdfe816
xiaoli <1135697882@qq.com>
2022-08-03 17:33:34 +0800

    第er天学习git

--- java01/src/main/java/com/itheima/com/Demo1.java
+++ java01/src/main/java/com/itheima/com/Demo1.java
@@ -2,7 +2,7 @@ package com.itheima.com;
 
 public class Demo1 {
     public String say(String name){
-        System.out.println("hello" + name);
+        System.out.println("hello" + name);//张三
         return "hello" + name;
     }
 }
